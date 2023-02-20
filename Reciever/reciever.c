c07e89a94c7c5abf384bfa0a703e35bc163c78a7
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2023-02-20 23:18:13 +0530

    Update reciever.c

--- Reciever/reciever.c
+++ Reciever/reciever.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #define MAX    32768
 #define MIN   -32768
+#define BMS_DATA 50
+#define BUFFER_SIZE 100
+
+
+void ReadFromConsoleAndStore(void)
+{
+    char buffers[BMS_DATA][BUFFER_SIZE];
+    int i;
+    FILE *output_file;
+
+    //printf("Enter %d strings, one per line:\n", NUM_BUFFERS);
+    for (i = 0; i < BMS_DATA; i++) {
+        fgets(buffers[i], BUFFER_SIZE, stdin);
+        // Remove newline character from buffer
+        buffers[i][strcspn(buffers[i], "\n")] = '\0';
+    }
+	
+    output_file = fopen("output.txt", "w");
+    if (output_file == NULL) {
+        printf("Error opening file for writing.\n");
+        return 1;
+    }
+    //printf("Read %d strings:\n", NUM_BUFFERS);
+    for (i = 0; i < BMS_DATA; i++) {
+        fprintf(output_file, "%s\n", buffers[i]);
+         printf("%s\n", buffers[i]);
+    }
+
+    
+}
 
 int getMax(int arr[])
 {
@@ -28,4 +60,8 @@ int getMin(int arr[])
         }
     }
     return min;
+}
+void main()
+{
+    ReadFromConsoleAndStore();
 }
